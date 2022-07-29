f3b319fda6a7053233fba9cbe22dca5349d53a54
josip0920 <eugene20920@gmail.com>
2022-07-29 09:20:15 +0900

    Create index.js

---
+++ src/config/index.js
@@ -0,0 +1,118 @@
+export const CardA_info = [
+    {
+        name: 'Heading of this Self Storage',
+        photo: "1.png",
+        location: 'Jersy City, NJ',
+        units: 89,
+        size: '40,233 Sq Ft',
+        occupancy: 50
+    },
+    {
+        name: 'Heading of this Self Storage',
+        photo: "2.png",
+        location: 'Jersy City, NJ',
+        units: 89,
+        size: '40,233 Sq Ft',
+        occupancy: 50
+    },
+    {
+        name: 'Heading of this Self Storage',
+        photo: "image 2.png",
+        location: 'Jersy City, NJ',
+        units: 89,
+        size: '40,233 Sq Ft',
+        occupancy: 50
+    },
+    {
+        name: 'Heading of this Self Storage',
+        photo: "image 7.png",
+        location: 'Jersy City, NJ',
+        units: 89,
+        size: '40,233 Sq Ft',
+        occupancy: 50
+    },
+    {
+        name: 'Heading of this Self Storage',
+        photo: "image 10.png",
+        location: 'Jersy City, NJ',
+        units: 89,
+        size: '40,233 Sq Ft',
+        occupancy: 50
+    },
+    {
+        name: 'Heading of this Self Storage',
+        photo: "image 11.png",
+        location: 'Jersy City, NJ',
+        units: 89,
+        size: '40,233 Sq Ft',
+        occupancy: 50
+    },
+    {
+        name: 'Heading of this Self Storage',
+        photo: "image 12.png",
+        location: 'Jersy City, NJ',
+        units: 89,
+        size: '40,233 Sq Ft',
+        occupancy: 50
+    },
+    {
+        name: 'Heading of this Self Storage',
+        photo: "image 14.png",
+        location: 'Jersy City, NJ',
+        units: 89,
+        size: '40,233 Sq Ft',
+        occupancy: 50
+    },
+]
+
+export const CardB_info = [
+
+    {
+        name: 'Heading of this Self Storage',
+        photo: "image 7.png",
+        location: 'Jersy City, NJ',
+        units: 89,
+        size: '40,233 Sq Ft',
+        occupancy: 50
+    },
+    {
+        name: 'Heading of this Self Storage',
+        photo: "image 10.png",
+        location: 'Jersy City, NJ',
+        units: 89,
+        size: '40,233 Sq Ft',
+        occupancy: 50
+    },
+    {
+        name: 'Heading of this Self Storage',
+        photo: "image 11.png",
+        location: 'Jersy City, NJ',
+        units: 89,
+        size: '40,233 Sq Ft',
+        occupancy: 50
+    },
+    {
+        name: 'Heading of this Self Storage',
+        photo: "image 12.png",
+        location: 'Jersy City, NJ',
+        units: 89,
+        size: '40,233 Sq Ft',
+        occupancy: 50
+    },
+    {
+        name: 'Heading of this Self Storage',
+        photo: "1.png",
+        location: 'Jersy City, NJ',
+        units: 89,
+        size: '40,233 Sq Ft',
+        occupancy: 50
+    },
+    {
+        name: 'Heading of this Self Storage',
+        photo: "image 14.png",
+        location: 'Jersy City, NJ',
+        units: 89,
+        size: '40,233 Sq Ft',
+        occupancy: 50
+    },
+]
